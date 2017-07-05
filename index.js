030f178a401daae9ee4837a33fb01ba31a01053f
techmmr <mmishra@grepruby.com>
2017-07-05 20:27:52 +0530

    init

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var express = require('express');
-var app = express();
+const path = require('path')
+const express = require('express')
+const app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
+// set the view engine to ejs
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
-
+app.listen(3000);
+console.log('3000 is the magic port');
